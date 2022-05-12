0dc94b3d712f4c9f88f9e619691354a624abf4a3
Buhari Nasir Maiwada <maewada@live.com>
2022-05-12 06:08:53 +0100

    do-while loop

--- src/com/bsoft/controlFlow/DoWhile.java
+++ src/com/bsoft/controlFlow/DoWhile.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class DoWhile {
     public static void main(String[] args) {
 
+        /*
+            The Java do-while loop is used to iterate a part of the program repeatedly, until the specified condition is true.
+            If the number of iteration is not fixed and you must have to execute the loop at least once,
+            it is recommended to use a do-while loop.
+        */
         Scanner sc = new Scanner(System.in);
         String input = "";
         do{
